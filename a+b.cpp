565d87239500e4ff1baeddec2792c69a309c99cb
lagoon0o0 <lagoonzxy@gmail.com>
2015-07-24 23:20:38 +0800

    i dont know

---
+++ a+b.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	return 0;
+}
